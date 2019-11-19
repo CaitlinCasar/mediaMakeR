0306c882fccde79cf7d9f38673e67bc8279bb584
Caitlin <casar@u.northwestern.edu>
2019-11-19 14:45:37 -0600

    updating html output

--- R/make_media_recipe.R
+++ R/make_media_recipe.R
@@ -12,37 +12,55 @@
 #   Build and Reload Package:  'Cmd + Shift + B'
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
-devtools::use_data(x, mtcars)
-save(ion_library, file="data/ion_library.RData")
+#devtools::use_data(x, mtcars)
+#save(ion_library, file="data/ion_library.RData")
 
 make_media_recipe <- function(data, TE_conc = 1000, base_conc = 10){
   `%>%` <- magrittr::`%>%`
   data("compound_library")
   data("ion_library")
-  data("env_data")
-  TE = env_data %>%
+  TE = data %>%
     dplyr::left_join(ion_library) %>%
     dplyr::filter(type == "trace") %>%
     dplyr::left_join(compound_library, by=c("ion" = "ion_a")) %>%
     dplyr::mutate(mass_ratio = (atomic_mass_grams*coeff_a)/molecular_mass_grams,
-                  compound_recipe_mass = (env_grams_per_L/mass_ratio)*TE_conc,
-                  recipe_mass_a = env_grams_per_L*TE_conc,
+                  compound_recipe_mass = (env_grams_per_L/mass_ratio)*1000*TE_conc,
+                  recipe_mass_a = env_grams_per_L*1000*TE_conc,
                   recipe_mass_b = compound_recipe_mass - recipe_mass_a,
                   unit = "mg",
                   concentration = paste(TE_conc, "X"))
-  base = env_data %>%
+  base = data %>%
     dplyr::left_join(ion_library) %>%
     dplyr::filter(type == "salt") %>%
     dplyr::left_join(compound_library, by=c("ion" = "ion_a")) %>%
     dplyr::mutate(mass_ratio = (atomic_mass_grams*coeff_a)/molecular_mass_grams,
-                  compound_recipe_mass = ((env_grams_per_L/mass_ratio)*base_conc)/1000,
-                  recipe_mass_a = (env_grams_per_L*base_conc)/1000,
+                  compound_recipe_mass = (env_grams_per_L/mass_ratio)*base_conc,
+                  recipe_mass_a = env_grams_per_L*base_conc,
                   recipe_mass_b = compound_recipe_mass - recipe_mass_a,
                   unit = "g",
                   concentration = paste(base_conc, "X"))
-  recipe = TE %>%
-    dplyr::bind_rows(base) %>%
-    dplyr::select(compound, compound_recipe_mass, unit, type, concentration)
-  DT::datatable(recipe)
-}
+  TErecipe = TE %>%
+    dplyr::select(compound, compound_recipe_mass, unit, type, concentration)%>%
+    dplyr::filter(!is.na(compound))
+  baseRecipe = base %>%
+    dplyr::select(compound, compound_recipe_mass, unit, type, concentration) %>%
+    dplyr::filter(!is.na(compound))
 
+ writeLines(
+   c(
+     "# Media Recipe",
+     "<h3 style='color:red'>Trace Element Solution</h3>",
+     "```{r, echo=F}",
+     'knitr::kable(TErecipe) %>%
+                kableExtra::kable_styling(c("hover","stripe"))',
+     "```",
+   "### Basal Media",
+   "```{r, echo=F}",
+   'knitr::kable(baseRecipe) %>%
+                kableExtra::kable_styling(c("hover","stripe"))',
+   "```"),
+   "recipe.Rmd"
+   )
+ rmarkdown::render("recipe.Rmd")
+ if (interactive()) browseURL("recipe.html")
+}
